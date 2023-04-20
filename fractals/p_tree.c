d80a520efbdaf210797719e27b4095609d99b44e
Kira Klingenberg <kirak@pdx.edu>
2023-04-20 00:24:37 -0700

    Working, functions need major name and abstraction cleanup. Flowers added

--- fractals/p_tree.c
+++ fractals/p_tree.c
@@ -34,19 +34,12 @@ struct Point calculate_point(struct Point p0, struct Point p1, double distance, 
     double y3 = pm.y + change_x;
     struct Point p3 = {x3, y3};
 
-    //TODO test we found P3 correctly, we did!
-    G_rgb(0,0,0); //black
-    G_line(p3.x, p3.y, p0.x, p0.y);
-
     //first find the distance between P1 and Pm
     double dis_p1_pm = sqrt((p1.x - pm.x) * (p1.x - pm.x) + (p1.y - pm.y) * (p1.y - pm.y));
-    printf("Distance p1 pm %f ", dis_p1_pm);
     //then find the distance between p0 and pm
     double dis_p0_pm = distance - dis_p1_pm;
-    printf("Distance p0 pm %f ", dis_p0_pm);
 
     double l = sqrt(dis_p1_pm * dis_p0_pm);
-    printf("l: %f ", l);
 
     double scale_factor = l / distance;
 
@@ -64,26 +57,111 @@ struct Point calculate_point(struct Point p0, struct Point p1, double distance, 
 
 }
 
+struct Point find_point(struct Point p0, struct Point p1){
+    double change_x = p1.x - p0.x;
+    double change_y = p1.y - p0.y;
+
+    double p2x = p1.x - change_y;
+    double p2y = p1.y + change_x;
+    struct Point p_off_p1 = {p2x, p2y};
+
+    return p_off_p1;
+}
+
 void tree (struct Point p0, struct Point p1, double distance, double percent, double depth){
     if (depth == 0){
          return;
     }
 
-    //get the thrid point
-    struct Point p3 = calculate_point(p0, p1, distance, percent);
+    //get the third point
+    struct Point p2 = calculate_point(p0, p1, distance, percent);
 
     //TODO: test, draw the triangle
     G_rgb(1,0,0); //red
-    G_line(p0.x, p0.y, p3.x, p3.y);
+    G_line(p0.x, p0.y, p2.x, p2.y);
     G_rgb(0,1,0); //green
-    G_line(p3.x, p3.y, p1.x, p1.y);
+    G_line(p2.x, p2.y, p1.x, p1.y);
     G_rgb(1,0,1); //purple
     G_line(p1.x, p1.y, p0.x, p0.y);
 
-    //draw squares off each edge: p0-p3, p3-p1, p1-p0
-    //change in x and change in y will be the same to find how far up and
+    //draw squares off each edge: p0-p2, p2-p1
+    double change_x = p2.x - p0.x;
+    double change_y = p2.y - p0.y;
+
+    //point off p2
+    double p4x = p2.x - change_y;
+    double p4y = p2.y + change_x;
+    struct Point p4 = {p4x, p4y};
+
+    //point off p0
+    double p3x = p0.x - change_y;
+    double p3y = p0.y + change_x;
+    struct Point p3 = {p3x, p3y};
+    //Draw square off p0-p2
+    G_rgb(0,0,0);
+    G_line(p2.x, p2.y, p4.x, p4.y);
+    G_line(p0.x, p0.y, p3.x, p3.y);
+    G_line(p3.x, p3.y, p4.x, p4.y);
+
+    change_x = p1.x - p2.x;
+    change_y = p1.y - p2.y;
+
+    //point off p2
+    double p5x = p2.x - change_y;
+    double p5y = p2.y + change_x;
+    struct Point p5 = {p5x, p5y};
+
+    //point off p1
+    double p6x = p1.x - change_y;
+    double p6y = p1.y + change_x;
+    struct Point p6 = {p6x, p6y};
+
+    //draw square off p2-p1
+    G_rgb(0,0,0);
+    G_line(p1.x, p1.y, p6.x, p6.y);
+    G_line(p2.x, p2.y, p5.x, p5.y);
+    G_line(p5.x, p5.y, p6.x, p6.y);
+
+
+    change_x = p0.x - p1.x;
+    change_y = p0.y - p1.y;
+
+    //point off p0
+    double p7x = p0.x - change_y;
+    double p7y = p0.y + change_x;
+    struct Point p7 = {p7x, p7y};
+
+    //point off p1
+    double p8x = p1.x - change_y;
+    double p8y = p1.y + change_x;
+    struct Point p8 = {p8x, p8y};
+
+    //Draw square off p0-p1
+    G_rgb(0,0,0);
+    G_line(p0.x, p0.y, p7.x, p7.y);
+    G_line(p1.x, p1.y, p8.x, p8.y);
+    G_line(p7.x, p7.y, p8.x, p8.y);
+
+
+    if(depth == 1){
+
+        G_rgb(1,0,1);
+        G_fill_circle(p3.x, p3.y, 5);
+        G_fill_circle(p4.x, p4.y, 5);
+        G_fill_circle(p5.x, p5.y, 5);
+        G_fill_circle(p6.x, p6.y, 5);
+
+    }
 
     depth -= 1;
+
+
+    //call children with p3-p4 and p5-p6
+    tree(p3,p4, distance, percent, depth);
+    tree(p5, p6, distance, percent, depth);
+
+
+
 }
 
 int main(){
@@ -93,7 +171,7 @@ int main(){
     int swidth, sheight ;
     double x0, y0, x1, y1, x2, y2, length, adj, opp;
 
-    swidth = 400 ;  sheight = 400 ;
+    swidth = 1000 ;  sheight = 1000 ;
     G_init_graphics (swidth,sheight) ;  // interactive graphics
 
     // clear the screen in a given color
@@ -103,17 +181,24 @@ int main(){
     /* CODE HERE */
 
     //Create initial p0 and p1 points for the first triangle (also top of trunk square)
-    struct Point p0 = {100, 100};
-    struct Point p1 = {300, 100};
+    struct Point p0 = {450, 100};
+    struct Point p1 = {550, 100};
     double distance = sqrt((p1.x - p0.x) * (p1.x - p0.x) + (p1.y - p0.y) * (p1.y - p0.y));
-    printf("Distance %f ", distance);
     //set how far from p0 we want the third point to lie
-    double percent = 0.3;
+    double percent = 0.4;
+    double depth = 8;
 
 
+    struct Point p2 = {200, 500};
+    struct Point p3 = {250, 500};
+    double distance2 = sqrt((p3.x - p2.x) * (p3.x - p2.x) + (p3.y - p2.y) * (p3.y - p2.y));
+    double percent2 = 0.7;
+    double depth2 = 7;
+
+    tree(p2,p3, distance2, percent2, depth2);
     //pass two points of a line to a function that calculates the missing third point, and draws a square off each edge
     //of the resulting triangle
-
+    tree(p0, p1, distance, percent, depth);
 
 
 
